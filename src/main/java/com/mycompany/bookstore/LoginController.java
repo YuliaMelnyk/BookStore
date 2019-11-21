e35586127ca4110852bda62c23834a6268c67179
YuliaMelnyk <57395943+YuliaMelnyk@users.noreply.github.com>
2019-11-21 13:25:30 +0100

    Added LoginController

--- src/main/java/com/mycompany/bookstore/LoginController.java
+++ src/main/java/com/mycompany/bookstore/LoginController.java
@@ -1,33 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.bookstore;
+
+import java.sql.SQLException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-/**
- *
- * @author serhiimelnyk
- */
-public class LoginController implements Initializable {
-
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }
+public class LoginController {
 
     @FXML
-    protected void onAction(ActionEvent event) {
+    private TextField txt_email;
+
+    @FXML
+    private PasswordField txt_password;
+
+    @FXML
+    private Button btn_signin;
+
+    @FXML
+    public void login(ActionEvent event) throws SQLException {
+
+        Window owner = btn_signin.getScene().getWindow();
+
+        System.out.println(txt_email.getText());
+        System.out.println(txt_password.getText());
+
+        if (txt_email.getText().isEmpty()) {
+            showAlert(AlertType.ERROR, owner, "Form Error!",
+                    "Please enter your email");
+            return;
+        }
+        if (txt_password.getText().isEmpty()) {
+            showAlert(AlertType.ERROR, owner, "Form Error!",
+                    "Please enter a password");
+            return;
+        }
+
+        String emailId = txt_email.getText();
+        String password = txt_password.getText();
+/*
+        JdbcDao jdbcDao = new JdbcDao();
+        boolean flag = jdbcDao.validate(emailId, password);*/
+
+       /* if (!flag) {
+            infoBox("Please enter correct Email and Password", null, "Failed");
+        } else {
+            infoBox("Login Successful!", null, "Failed");
+        }*/
     }
 
+    public static void infoBox(String infoMessage, String headerText, String title) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    }
+
+    private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
 }
