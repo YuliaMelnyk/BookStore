261a1107f754780b3178d102e31b1df5c79df3cb
YuliaMelnyk <57395943+YuliaMelnyk@users.noreply.github.com>
2019-11-25 11:14:26 +0100

    Added AdminPageController, AdminPageDao, AdminPage

---
+++ src/main/java/Dao/AdminPageDao.java
@@ -0,0 +1,57 @@
+package Dao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminPageDao {
+    private static final String URL = "jdbc:mysql://85.214.120.213:3306/bookstoredam";
+    private static final String NAME = "bookstore";
+    private static final String PASSWORD = "1dam";
+
+    private static final String QUERY = "INSERT INTO book (isbn , name, genre, price, description, author, publisher, year, language) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+
+    public void insertBook(String isbn, String name, String genre, String price, String description, String author, String publisher,
+    String year, String language) throws SQLException {
+
+        try (Connection connection = DriverManager
+                .getConnection(URL, NAME, PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(QUERY)) {
+            preparedStatement.setString(1, isbn);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, genre);
+            preparedStatement.setString(4, price);
+            preparedStatement.setString(5, description);
+            preparedStatement.setString(6, author);
+            preparedStatement.setString(7, publisher);
+            preparedStatement.setString(8, year);
+            preparedStatement.setString(9, language);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    public static void printSQLException(SQLException ex) {
+        getException(ex);
+    }
+
+    static void getException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                Throwable p = ex.getCause();
+                while (p != null) {
+                    System.out.println(p);
+                    p = p.getCause();
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/mycompany/bookstore/AdminPageController.java
@@ -0,0 +1,89 @@
+package com.mycompany.bookstore;
+
+import Dao.SignUpDao;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+
+
+public class AdminPageController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    //inject  values defined in an FXML file into references
+
+    @FXML
+    private TextField txt_name;
+
+    @FXML
+    private TextField txt_email;
+
+    @FXML
+    private PasswordField txt_password;
+
+    @FXML
+    private TextField txt_address;
+
+    @FXML
+    private TextField txt_phone;
+
+    @FXML
+    private Button btn_signUp;
+
+    //on Click event method
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) throws SQLException {
+        Window owner = btn_signUp.getScene().getWindow();
+        if (txt_name.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter your name");
+            return;
+        }
+        if (txt_email.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter your email");
+            return;
+        }
+
+        if (txt_password.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter a password");
+            return;
+        }
+        if (txt_address.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter an address");
+            return;
+        }
+        if (txt_phone.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!",
+                    "Please enter a phone");
+            return;
+        }
+        // Saving our values in local variables
+
+        String name = txt_name.getText();
+        String email = txt_email.getText();
+        String password = txt_password.getText();
+        String address = txt_address.getText();
+        String phone = txt_phone.getText();
+
+        //Create an object Dao and call his method to put in database our local variables
+
+        SignUpDao signUpDao = new SignUpDao();
+        signUpDao.insertRecord(name, email, password, address, phone);
+        showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!",
+                "Welcome " + txt_name.getText());
+    }
